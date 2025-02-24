768e70e353e39cb3025d503cfcbc3df8489944f8
Kota Yuhara <hotwater1367@gmail.com>
2025-02-24 18:47:51 +0900

    init

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type    = string
+  default = "mapstack"
+}
+
+variable "project_id" {
+  type    = string
+  default = "mapstack-451909"
+}
+
+variable "region" {
+  type    = string
+  default = "asia-northeast1"
+}
